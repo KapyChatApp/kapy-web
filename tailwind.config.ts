db9730d9b8273023a14a47e362a90031455a3c1a
Vhquynhanh <22520086@gm.uit.edu.vn>
2024-09-12 14:47:32 +0700

    Set up project

---
+++ tailwind.config.ts
@@ -0,0 +1,80 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          500: "#F57206",
+          100: "#FFAB66",
+        },
+        dark: {
+          100: "#000000",
+          200: "#131313",
+          300: "#2B2B2B",
+          400: "#2E333D",
+          500: "#4C4C4C",
+          600: "#666666",
+        },
+        light: {
+          900: "#FFFFFF",
+          850: "#F5F5F5",
+          800: "#F0F0F0",
+          700: "#E7E7E7",
+          500: "#DBDBDB",
+          600: "#CCCCCC",
+        },
+        "accent-red": "#EA4335",
+        "accent-blue": "#4285F4",
+      },
+      fontFamily: {
+        sans: ["Helvetica", "Arial", "sans-serif"],
+      },
+      boxShadow: {
+        "light-100":
+          "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px rgba(184, 184, 184, 0.02), 0px 2px 4px 0px rgba(184, 184, 184, 0.03)",
+        "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
+        "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+      },
+      backgroundImage: {
+        "auth-dark": "url('/assets/images/auth-dark.png')",
+        "auth-light": "url('/assets/images/auth-light.png')",
+      },
+      screens: {
+        xs: "420px",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+};
