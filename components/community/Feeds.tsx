c8af03cb2b50ee62b24f1c693099d25fe55731e1
Vhquynhanh <vhqa2004@gmail.com>
2025-03-05 13:10:25 +0700

    Content & Caption

--- components/community/Feeds.tsx
+++ components/community/Feeds.tsx
@@ -16,7 +16,48 @@ const detailPost: PostResponseDTO = {
   comments: [],
   caption: "hello",
   createAt: "2025-01-02T04:47:05.847+00:00",
-  contents: []
+  contents: [
+    {
+      _id: "1",
+      fileName: "avatar.png",
+      url: "https://res.cloudinary.com/dtn9r75b7/image/upload/v1735733280/Avatar/ghlgwprdxd1jxlus3arx.png",
+      bytes: 102400,
+      width: 500,
+      height: 500,
+      format: "png",
+      type: "Image"
+    },
+    {
+      _id: "2",
+      fileName: "video.mp3",
+      url: "https://res.cloudinary.com/dtn9r75b7/video/upload/v1735799193/Videos/cy0s5a4ljaipis4xk3io.mov",
+      bytes: 102400,
+      width: 500,
+      height: 500,
+      format: "MOV",
+      type: "Video"
+    },
+    {
+      _id: "3",
+      fileName: "tests-example.xls",
+      url: "https://res.cloudinary.com/dtn9r75b7/raw/upload/v1735796263/Documents/Documents/tests-example.xls",
+      bytes: 102400,
+      width: 500,
+      height: 500,
+      format: "xls",
+      type: "Other"
+    },
+    {
+      _id: "3",
+      fileName: "7.mp3.m4a",
+      url: "https://res.cloudinary.com/dtn9r75b7/video/upload/v1735801570/Audios/Audios/7.mp3.m4a",
+      bytes: 102400,
+      width: 500,
+      height: 500,
+      format: "m4a",
+      type: "Audio"
+    }
+  ]
 };
 const Feeds = () => {
   const [isClickRecent, setIsClickRecent] = useState(true);
